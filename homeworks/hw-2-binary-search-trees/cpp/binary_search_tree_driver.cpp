a86397b81654b111971f3e5cdda04b051c150028
Alec Thilenius <alec@thilenius.com>
2013-01-30 17:13:39 -0700

    Fixed bug with last checkin.

--- homeworks/hw-2-binary-search-trees/cpp/binary_search_tree_driver.cpp
+++ homeworks/hw-2-binary-search-trees/cpp/binary_search_tree_driver.cpp
@@ -234,7 +234,7 @@ TEST_BEGIN("Remove")
 		top->right != NULL				&& top->right->data == 5 &&
 		top->left != NULL				&& top->left->data == 3 &&
 		top->left->left != NULL			&& top->left->left->data == 2 &&
-		top->left->left->left != NULL	&& top->left->left->left->data == 0 &&
+		top->left->left->left != NULL	&& top->left->left->left->data == 0;
 
 		IsTrue("Remove Trunk", result1 || result2 || result3, 
 		"Failed to remove the trunk ( branch with two children ).");
