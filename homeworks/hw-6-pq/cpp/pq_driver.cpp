597772a9a49ceca657cbf965a3fc6374f9be0414
Gabe Johnson <gabe.johnson@gmail.com>
2013-03-08 14:59:17 -0700

    Merry Christmas.

--- homeworks/hw-6-pq/cpp/pq_driver.cpp
+++ homeworks/hw-6-pq/cpp/pq_driver.cpp
@@ -10,6 +10,8 @@
 //
 // The test code will appear on RetroGrade late in the game.
 
+#include <string>
+#include <iostream>
 #include "UTFramework.h"
 #include "pq.h"
 
@@ -17,10 +19,58 @@ using namespace Thilenius;
 
 SUITE_BEGIN("Priority Queue")
 
-TEST_BEGIN("SanityCheck")
+TEST_BEGIN("Everything")
 {
-  IsTrue("Sane?", true, "OK I'm insane");
+  pq* queue = init_priority_queue();
+  string text = "Cat Video";
+  insert(queue, text, 3);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "Cat Video", "Why am I not watching a cat video right now?");
+  text = "RTS Game Packet";
+  insert(queue, text, 10);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "RTS Game Packet", "Game Packet Not Found");
+  text = "PingAlive";
+  insert(queue, text, 2);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "RTS Game Packet", "Game Packet Not Found");
+  text = "Medical Packet";
+  insert(queue, text, 11.5);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "Medical Packet", "Robot surgeon asleep at the wheel?");  
+  text = "Medical Packet";
+  insert(queue, text, 12.5);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "Medical Packet", "Robot surgeon asleep at the wheel?");  
+  text = "Cat Video";
+  insert(queue, text, 3);
+  text = peek(queue);
+  IsTrue("Peek OK", text == "Medical Packet", "Robot surgeon asleep at the wheel?");
+  // the PQ table should look like this:
+  // 2    Ping Alive
+  // 3    Cat Video
+  // 3    Cat Video
+  // 10   RTS Game Packet
+  // 11.5 Medical Packet
+  // 12.5 Medical Packet
+  text = remove(queue);
+  IsTrue("Remove OK", text == "Medical Packet", "Remove not working");
+  text = peek(queue);
+  IsTrue("Peek OK", text == "Medical Packet", "Robot surgeon asleep at the wheel?");
+  text = remove(queue);
+  IsTrue("Remove OK", text == "Medical Packet", "Remove not working");
+  text = peek(queue);
+  IsTrue("Peek OK", text == "RTS Game Packet", "Game Packet Not Found");  
+  text = remove(queue);
+  IsTrue("Remove OK", text == "RTS Game Packet", "Remove not working");  
+  text = remove(queue);
+  IsTrue("Remove OK", text == "Cat Video", "Remove not working");  
+  text = remove(queue);
+  IsTrue("Remove OK", text == "Cat Video", "Remove not working");  
+  text = remove(queue);
+  IsTrue("Remove OK", text == "PingAlive", "Remove not working");
 }TEST_END
+
 
 SUITE_END
 
